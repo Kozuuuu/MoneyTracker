5d1c6df11dd6c27a1be49931ec17a36dd026c148
kozuuuu <06jaysang2002@gmail.com>
2024-09-10 16:31:53 +0800

    "created new DB 'expenses_savings'"

--- MoenyTracker.session.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO financial_records (cash, date, expenses, saving, extra, period)
-VALUES 
-    (9228.10, '2024-07-31', 9228.10, 0, 0, 'July 15-31, 2024'),
-    (17130.95, '2024-08-15', 17130.95, 0, 0, 'August 1-15, 2024'),
-    (13407.44, '2024-08-30', 13407.44, 0, 0, 'August 16-30, 2024');
--- MySQL.sql
+++ MySQL.sql
@@ -17,3 +17,16 @@ VALUES
     (17130.95, '2024-08-15', 17130.95, 0, 0, 'August 1-15, 2024'),
     (13407.44, '2024-08-30', 13407.44, 0, 0, 'August 16-30, 2024');
 
+
+
+===================================== FOR expenses_savings TABLE
+
+create table if not exists expenses_savings (
+	no INT AUTO_INCREMENT PRIMARY KEY,
+    expenses DECIMAL(10, 2),
+    expenses_description VARCHAR(200),
+    monthly_payments DECIMAL (10, 2),
+    savings DECIMAL(10, 2),
+    savings_description VARCHAR(200),
+    total_savings DECIMAL(10, 2)
+);
