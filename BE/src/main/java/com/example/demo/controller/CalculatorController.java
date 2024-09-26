57b0907eddebde741472791a6f96c854c6905c98
kozuuuu <06jaysang2002@gmail.com>
2024-09-26 16:40:28 +0800

    Implemented Configuration for FE Connection

--- BE/src/main/java/com/example/demo/controller/CalculatorController.java
+++ BE/src/main/java/com/example/demo/controller/CalculatorController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ public class CalculatorController {
     @Autowired
     private CalculatorService calculatorService;
 
+    @CrossOrigin(origins = "http://localhost:9000")
     @PostMapping // ("/calculate")
     public CalculationResponse calculateExtra(@RequestBody Calculator input) {
         return calculatorService.calculateExtra(input);
